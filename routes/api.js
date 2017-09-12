7b3023a715536d860b1163402714131434256623
Aron Adler <aron@smartowldesign.com>
2017-09-12 22:29:57 +0100

    Add basic API endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { calculateSavings } = require("../helpers/savings");
 
-router.post('/savings', (req, res) => {
-    const { test } = req.body;
-    res.send(req.body);
+router.post("/savings", (req, res) => {
+  const {
+    principal,
+    interestRate,
+    interestAnnFreq,
+    monthlyDeposit,
+    monthsNum,
+    currency
+  } = req.body;
+
+  const paramsArray = [
+    principal,
+    interestRate,
+    interestAnnFreq,
+    monthlyDeposit,
+    monthsNum
+  ];
+
+  if (paramsArray.includes(undefined)) {
+    res.send({ error: "all parameters should be present" });
+  } else {
+    const savings = calculateSavings(...paramsArray);
+    res.send({ savings });
+  }
 });
 
 module.exports = router;
